0cb6fe9561f85aa8dc9cfd04c1013f008c7fe549
Antonio Kim <antoniok@sfu.ca>
2024-07-04 09:08:15 -0700

    Adding RestDTO and LinkDTO for HATEOAS

---
+++ GroceryStoreApi/DTO/LinkDTO.cs
@@ -0,0 +1,15 @@
+namespace GroceryStoreApi.DTO;
+
+public class LinkDTO
+{
+	public string Href { get; private set; }
+	public string Rel { get; private set; }
+	public string Type { get; private set; }
+
+	public LinkDTO(string href, string rel, string type)
+	{
+		Href = href;
+		Rel = rel;
+		Type = type;
+	}
+}
---
+++ GroceryStoreApi/DTO/RestDTO.cs
@@ -0,0 +1,7 @@
+namespace GroceryStoreApi.DTO;
+
+public class RestDTO<T>
+{
+	public List<LinkDTO> Links { get; set; } = new List<LinkDTO>();
+	public T Data { get; set; } = default!;
+}
