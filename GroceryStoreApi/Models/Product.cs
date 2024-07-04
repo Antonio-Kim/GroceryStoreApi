7a51022687f53cc42f05a819a12e968abcd9ed70
Antonio Kim <antoniok@sfu.ca>
2024-07-04 09:59:37 -0700

    Initializing Entity for Product

---
+++ GroceryStoreApi/Models/Product.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace GroceryStoreApi.Models;
+
+[Table("Products")]
+public class Product
+{
+	[Key]
+	[Required]
+	public int Id { get; set; }
+	[Required]
+	[MaxLength(50)]
+	public string? Category { get; set; }
+	[Required]
+	[MaxLength(100)]
+	public string? Name { get; set; }
+	[Required]
+	public bool InStock { get; set; }
+	[Required]
+	[Precision(6, 2)]
+	public decimal Price { get; set; }
+	[Required]
+	public int CurrentStock { get; set; }
+}
